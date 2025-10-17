5856bf6adcfc3e35eb7972de80a62d4343563bd0
Márcio Bortoti Júnior. <marcio.santo.junior@escola.pr.gov.br>
2025-10-17 10:35:34 -0300

    Create arquivo.py

---
+++ arquivo.py
@@ -0,0 +1,12 @@
+# Exercício 1.
+with open('perfil.txt','w') as conteudo:
+    conteudo.write("Márcio Bortoti do Espirito Santo Júnior.\n")
+    conteudo.write("Idade: 16\n")
+    conteudo.write("Comida Favorita: Frango\n")
+print("Arquivo funcionando perfeitamente.")
+#Exercício 2.
+with open('perfil.txt','r') as arquivo:
+    conteudo = arquivo.read()
+print("----Usando read() para fazer a leitura---------")
+print(conteudo)
+#
