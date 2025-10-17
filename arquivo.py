f774889e0a1e7fc71f1794b5a8aaa127414b78fe
Márcio Bortoti Júnior. <marcio.santo.junior@escola.pr.gov.br>
2025-10-17 16:00:33 -0300

    Add file writing and reading functionality

--- arquivo.py
+++ arquivo.py
@@ -1,12 +1,13 @@
 # Exercício 1.
+print("Exercício 1:")
 with open('perfil.txt','w') as conteudo:
     conteudo.write("Márcio Bortoti do Espirito Santo Júnior.\n")
     conteudo.write("Idade: 16\n")
     conteudo.write("Comida Favorita: Frango\n")
 print("Arquivo funcionando perfeitamente.")
 #Exercício 2.
+print("Exercício 2:")
 with open('perfil.txt','r') as arquivo:
     conteudo = arquivo.read()
 print("----Usando read() para fazer a leitura---------")
 print(conteudo)
-#
